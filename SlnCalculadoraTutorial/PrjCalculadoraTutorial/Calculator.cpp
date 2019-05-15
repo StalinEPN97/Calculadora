804d4b049aae0eb220a594afcb9956e4d6cd93bb
Stalin Collahuazo <stalinmh_leo97@hotmail.com>
2019-05-15 11:02:39 -0500

    cambio3

--- SlnCalculadoraTutorial/PrjCalculadoraTutorial/Calculator.cpp
+++ SlnCalculadoraTutorial/PrjCalculadoraTutorial/Calculator.cpp
@@ -2,16 +2,52 @@
 #include "Calculator.h"
 
 
-//Calculator::Calculator()
-//{
-//}
+/*Calculator::Calculator()
+{
+}
 
 
-//Calculator::~Calculator()
-//{
-//}
+Calculator::~Calculator()
+{
+}*/
+
+double Calculator::Suma(double x, double y)
+{
+	return x + y;
+}
+double Calculator::Resta(double x, double y)
+{
+	return x - y;
+}
+double Calculator::Multiplicacion(double x, double y)
+{
+	return x * y;
+}
+double Calculator::Division(double x, double y)
+{
+	return x / y;
+}
 
 double Calculator::Calculate(double x, char oper, double y)
 {
-	return 0.0;
+	double resultado=0;
+	switch (oper)
+	{
+	case '+':
+		resultado = x + y;
+		break;
+	case '-':
+		resultado = x - y;
+		break;
+	case '*':
+		resultado = x * y;
+		break;
+	case '/':
+		resultado = x / y;
+		break;
+	default:
+		resultado = 0;
+		break;
+	}
+	return resultado;
 }
--- SlnCalculadoraTutorial/PrjCalculadoraTutorial/PrjCalculadoraTutorial.cpp
+++ SlnCalculadoraTutorial/PrjCalculadoraTutorial/PrjCalculadoraTutorial.cpp
@@ -1,11 +1,23 @@
 #include"pch.h"
+#include "Calculator.h"
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
+	double valor1=0, valor2=0, valor3=0;
+	char operacion = '\0';
+
 	cout << "Calculator Console Application" << endl << endl;
-	cout << "Please enter the operation to perform. Format: a+b | a-b | a*b | a/b" << endl;
+	cout << "Porfavor ingrese la operacion en este formato: a+b | a-b | a*b | a/b" << endl;
+	while (true)
+	{
+		cin >> valor1 >> operacion >> valor2;
+		//cout << x + y << endl;
+		Calculator calcular;
+		valor3 = calcular.Calculate(valor1, operacion, valor2);
+		cout <<"El resultado es " << valor3 << endl;
+	}
 	return 0;
 }
